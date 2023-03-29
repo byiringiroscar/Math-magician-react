8343cde893ea89ab51029cb70e45c33b75bba7ed
byiringiroscar <byiringoroscar@gmail.com>
2023-03-29 13:47:33 +0200

    Create calculater interface

---
+++ src/component/screen.js
@@ -0,0 +1,26 @@
+const Screen = () => (
+  <div className="calculator-content">
+    <p className="calc-item item-one">0</p>
+    <div className="calc-item">0</div>
+    <div className="calc-item">AC</div>
+    <div className="calc-item">+/-</div>
+    <div className="calc-item operator">/</div>
+    <div className="calc-item">7</div>
+    <div className="calc-item">8</div>
+    <div className="calc-item">9</div>
+    <div className="calc-item operator">*</div>
+    <div className="calc-item">4</div>
+    <div className="calc-item">5</div>
+    <div className="calc-item">6</div>
+    <div className="calc-item operator">-</div>
+    <div className="calc-item">1</div>
+    <div className="calc-item">2</div>
+    <div className="calc-item">3</div>
+    <div className="calc-item operator">+</div>
+    <div className="calc-item item-zero">0</div>
+    <div className="calc-item">.</div>
+    <div className="calc-item operator">=</div>
+  </div>
+);
+
+export default Screen;
