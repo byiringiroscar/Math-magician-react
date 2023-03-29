d854d7e2e20a508849eb64075cdc99bd5b9be129
byiringiroscar <byiringoroscar@gmail.com>
2023-03-29 21:42:30 +0200

    Add calculator functionality and Change div to button

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,4 +1,6 @@
 import Screen from './screen';
+// import calculate from '../logic/calculate';
+// import operate from '../logic/operate';
 
 const Calculator = () => (
   <div className="single-pad">
--- src/component/screen.js
+++ src/component/screen.js
@@ -1,25 +1,25 @@
 const Screen = () => (
   <div className="calculator-content">
     <p className="calc-item item-one">0</p>
-    <div className="calc-item">0</div>
-    <div className="calc-item">AC</div>
-    <div className="calc-item">+/-</div>
-    <div className="calc-item operator">/</div>
-    <div className="calc-item">7</div>
-    <div className="calc-item">8</div>
-    <div className="calc-item">9</div>
-    <div className="calc-item operator">*</div>
-    <div className="calc-item">4</div>
-    <div className="calc-item">5</div>
-    <div className="calc-item">6</div>
-    <div className="calc-item operator">-</div>
-    <div className="calc-item">1</div>
-    <div className="calc-item">2</div>
-    <div className="calc-item">3</div>
-    <div className="calc-item operator">+</div>
-    <div className="calc-item item-zero">0</div>
-    <div className="calc-item">.</div>
-    <div className="calc-item operator">=</div>
+    <button type="button" className="calc-item">0</button>
+    <button type="button" className="calc-item">AC</button>
+    <button type="button" className="calc-item">+/-</button>
+    <button type="button" className="calc-item operator">/</button>
+    <button type="button" className="calc-item">7</button>
+    <button type="button" className="calc-item">8</button>
+    <button type="button" className="calc-item">9</button>
+    <button type="button" className="calc-item operator">*</button>
+    <button type="button" className="calc-item">4</button>
+    <button type="button" className="calc-item">5</button>
+    <button type="button" className="calc-item">6</button>
+    <button type="button" className="calc-item operator">-</button>
+    <button type="button" className="calc-item">1</button>
+    <button type="button" className="calc-item">2</button>
+    <button type="button" className="calc-item">3</button>
+    <button type="button" className="calc-item operator">+</button>
+    <button type="button" className="calc-item item-zero">0</button>
+    <button type="button" className="calc-item">.</button>
+    <button type="button" className="calc-item operator">=</button>
   </div>
 );
 
