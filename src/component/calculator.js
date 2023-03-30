23d45858005486fe4d284b946d635a8ef17b4ea0
byiringiroscar <byiringoroscar@gmail.com>
2023-03-30 09:26:46 +0200

    Add calculator functionality

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,11 +1,10 @@
 import Screen from './screen';
-// import calculate from '../logic/calculate';
-// import operate from '../logic/operate';
 
 const Calculator = () => (
   <div className="single-pad">
     <Screen />
   </div>
+
 );
 
 export default Calculator;
--- src/component/screen.js
+++ src/component/screen.js
@@ -1,26 +1,42 @@
-const Screen = () => (
-  <div className="calculator-content">
-    <p className="calc-item item-one">0</p>
-    <button type="button" className="calc-item">0</button>
-    <button type="button" className="calc-item">AC</button>
-    <button type="button" className="calc-item">+/-</button>
-    <button type="button" className="calc-item operator">/</button>
-    <button type="button" className="calc-item">7</button>
-    <button type="button" className="calc-item">8</button>
-    <button type="button" className="calc-item">9</button>
-    <button type="button" className="calc-item operator">*</button>
-    <button type="button" className="calc-item">4</button>
-    <button type="button" className="calc-item">5</button>
-    <button type="button" className="calc-item">6</button>
-    <button type="button" className="calc-item operator">-</button>
-    <button type="button" className="calc-item">1</button>
-    <button type="button" className="calc-item">2</button>
-    <button type="button" className="calc-item">3</button>
-    <button type="button" className="calc-item operator">+</button>
-    <button type="button" className="calc-item item-zero">0</button>
-    <button type="button" className="calc-item">.</button>
-    <button type="button" className="calc-item operator">=</button>
-  </div>
-);
+import { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const Screen = () => {
+  const [calcul, setCalcul] = useState({ total: 0, next: null, operation: null });
+  const handleClick = (e) => {
+    const data = e.target.textContent;
+    const calc = calculate(calcul, data);
+    setCalcul(calc);
+  };
+  const { total, next, operation } = calcul;
+  return (
+    <div className="calculator-content">
+      <p className="calc-item item-one">{ next || operation || total || 0}</p>
+      <button type="button" className="calc-item" onClick={handleClick}>0</button>
+      <button type="button" className="calc-item" onClick={handleClick}>AC</button>
+      <button type="button" className="calc-item" onClick={handleClick}>+/-</button>
+      <button type="button" className="calc-item operator" onClick={handleClick} value="&divide;">/</button>
+      <button type="button" className="calc-item" onClick={handleClick}>7</button>
+      <button type="button" className="calc-item" onClick={handleClick}>8</button>
+      <button type="button" className="calc-item" onClick={handleClick}>9</button>
+      <button type="button" className="calc-item operator" onClick={handleClick}>*</button>
+      <button type="button" className="calc-item" onClick={handleClick}>4</button>
+      <button type="button" className="calc-item" onClick={handleClick}>5</button>
+      <button type="button" className="calc-item" onClick={handleClick}>6</button>
+      <button type="button" className="calc-item operator" onClick={handleClick}>-</button>
+      <button type="button" className="calc-item" onClick={handleClick}>1</button>
+      <button type="button" className="calc-item" onClick={handleClick}>2</button>
+      <button type="button" className="calc-item" onClick={handleClick}>3</button>
+      <button type="button" className="calc-item operator" onClick={handleClick}>+</button>
+      <button type="button" className="calc-item item-zero" onClick={handleClick}>0</button>
+      <button type="button" className="calc-item" onClick={handleClick}>.</button>
+      <button type="button" className="calc-item operator" onClick={handleClick}>=</button>
+    </div>
+  );
+};
+
+// Screen.propTypes = {
+//   handleClick: PropTypes.func.isRequired,
+// };
 
 export default Screen;
